c9646c99c55e8e3db0a9bb36ec6d08c79537958c
Yves Lardenoije <yveslardenoije@gmail.com>
2024-10-17 07:12:57 +0200

    BallDetector

---
+++ PSocInterface/AIPlayer/Main.py
@@ -0,0 +1,11 @@
+from IFlipper import Flipper
+
+
+
+if __name__ == "__main__":
+    # Create a DeviceInterface object
+    DI = DI()
+    # Create a Flipper object
+    flipper = Flipper(1,DI)
+    # Call the flip method
+    flipper.flip()
